8b88043d473e542fa5274351f237ddc02f353e37
Karan Gathani <karan.gathani@posit.co>
2024-10-23 11:48:48 -0700

    add github workflow

--- README.md
+++ README.md
@@ -8,9 +8,11 @@
 Create shiny issues and get AI responses on GitHub
 ---
 
-Common responses include:
+Includes all the crowd favorite responses like:
 - `I am sorry, I am not able to help you with that.`
 - `Please hold while I transfer you to the appropriate department.`
 - `I'll be sure to pass along your feedback to the appropriate team.`
 - `I understand your frustration, let me see what I can do.`
 - `Please allow me a moment to review your account.`
+
+    And more...
---
+++ process_issue.py
@@ -0,0 +1,32 @@
+from playwright.sync_api import sync_playwright, Playwright
+import time
+import os
+
+
+def run(playwright: Playwright):
+    ISSUE_BODY = os.environ["ISSUE_BODY"]
+    chromium = playwright.chromium
+    browser = chromium.launch()
+    page = browser.new_page()
+    page.goto("https://gallery.shinyapps.io/assistant")
+    page.get_by_label("Python").check()
+    page.get_by_role("textbox", name="Enter a message...").click()
+    page.get_by_role("textbox", name="Enter a message...").fill(
+        f"""
+Help me with answer this customer query. Respond like a professional support agent and say when you don't have any answer instead of providing an incorrect one.
+Also, let them know these answers are AI generated and can have errors -
+{ISSUE_BODY}
+"""
+    )
+    page.get_by_label("Send message").click()
+    time.sleep(12)  # required since we are streaming responses
+    message_contents = page.query_selector_all(".message-content")
+    last_message_content = message_contents[-1].text_content()
+    print(last_message_content)
+    with open(os.environ['GITHUB_OUTPUT'], 'w') as f:
+        print(response=last_message_content, file=f)
+    browser.close()
+
+
+with sync_playwright() as playwright:
+    run(playwright)
